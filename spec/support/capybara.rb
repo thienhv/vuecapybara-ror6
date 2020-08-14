2256cc521f54157fa81025757ca690f46dbe4a7c
Thien Ho <thien.hoviet@gmail.com>
2020-08-14 18:27:44 +0700

    Config capybara

---
+++ spec/support/capybara.rb
@@ -0,0 +1,20 @@
+require 'capybara/rails'
+require "selenium/webdriver"
+
+Capybara.javascript_driver = :webkit
+
+Capybara.register_driver :chrome do |app|
+  Capybara::Selenium::Driver.new(app, browser: :chrome)
+end
+
+Capybara.register_driver :headless_chrome do |app|
+  capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
+    chromeOptions: { args: %w(headless disable-gpu) }
+  )
+
+  Capybara::Selenium::Driver.new app,
+    browser: :chrome,
+    desired_capabilities: capabilities
+end
+
+Capybara.javascript_driver = :headless_chrome
