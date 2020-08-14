9f8121527c951244f703a130d42a4e2af20222a3
Thien Ho <thien.hoviet@gmail.com>
2020-08-14 18:28:14 +0700

    Add test

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,3 +15,19 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+import Vue from 'vue/dist/vue.esm';
+import Employees from './Employees.vue';
+
+document.addEventListener('turbolinks:load', () => {
+   var element = document.querySelector('#employees');
+
+  if (element !== null) {
+    var employees = new Vue({
+      components: {
+        'employees': Employees
+      },
+      el: element
+    });
+  }
+});
---
+++ spec/factories/employees.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :employee do
+    name "MyString"
+    age 1
+  end
+end
